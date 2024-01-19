869d563624237f0f318840e802a912b22a57b94f
michaelgardner87 <michael.gardner@axiomzen.co>
2024-01-19 14:55:41 -0800

    add overides

--- main.tf
+++ main.tf
@@ -31,21 +31,21 @@ resource "cloudflare_ruleset" "zone_level_managed_waf" {
     action_parameters {
       id      = "4814384a9e5d4991b9815dcfc25d2f1f"
       version = "latest"
-      #      overrides {
-      #        categories {
-      #          category = "paranoia-level-3"
-      #          enabled  = false
-      #        }
-      #        categories {
-      #          category = "paranoia-level-4"
-      #          enabled  = false
-      #        }
-      #        rules {
-      #          id              = "6179ae15870a4bb7b2d480d4843b323c"
-      #          action          = "managed_challenge"
-      #          score_threshold = 25
-      #        }
-      #      }
+      overrides {
+        categories {
+          category = "paranoia-level-3"
+          enabled  = false
+        }
+        categories {
+          category = "paranoia-level-4"
+          enabled  = false
+        }
+        rules {
+          id              = "6179ae15870a4bb7b2d480d4843b323c"
+          action          = "managed_challenge"
+          score_threshold = 25
+        }
+      }
     }
     expression  = "true"
     description = "Execute Cloudflare Managed Ruleset on my zone-level phase entry point ruleset"
