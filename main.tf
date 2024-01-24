43e61555deeaa11e687983cc80b88718dd6b2d8b
michaelgardner87 <michael.gardner@axiomzen.co>
2024-01-23 16:20:50 -0800

    add managed ruleset

--- main.tf
+++ main.tf
@@ -26,6 +26,18 @@ resource "cloudflare_ruleset" "zone_level_managed_waf" {
   kind        = "zone"
   phase       = "http_request_firewall_managed"
 
+  # Managed Ruleset
+  rules {
+    action = "execute"
+    action_parameters {
+      id = "efb7b8c949ac4650a09736fc376e9aee"
+    }
+    expression  = "true"
+    description = "Execute Cloudflare Managed Ruleset on my zone-level phase entry point ruleset"
+    enabled     = true
+  }
+
+  # OWASP Core Ruleset
   rules {
     action = "execute"
     action_parameters {
