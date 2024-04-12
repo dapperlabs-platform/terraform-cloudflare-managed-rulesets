e8a0b9468b382cb55d37c0f2fecb68f6c7c2d911
michaelgardner87 <michael.gardner@axiomzen.co>
2024-04-12 09:43:47 -0700

    add set paranoia level based on number

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+locals {
+  category_settings = {
+    "1" = [
+      { category = "paranoia-level-2", enabled = false },
+      { category = "paranoia-level-3", enabled = false },
+      { category = "paranoia-level-4", enabled = false }
+    ],
+    "2" = [
+      { category = "paranoia-level-3", enabled = false },
+      { category = "paranoia-level-4", enabled = false }
+    ],
+    "3" = [
+      { category = "paranoia-level-4", enabled = false }
+    ],
+    "4" = []
+  }
+}
+
+
+
 data "cloudflare_zones" "zones" {
   count = length(var.domains)
 
@@ -7,6 +27,7 @@ data "cloudflare_zones" "zones" {
     paused      = false
   }
 }
+
 
 resource "cloudflare_ruleset" "zone_level_managed_waf" {
   count = length(var.domains)
@@ -35,22 +56,14 @@ resource "cloudflare_ruleset" "zone_level_managed_waf" {
       id      = "4814384a9e5d4991b9815dcfc25d2f1f"
       version = "latest"
       overrides {
-        categories {
-          category = "paranoia-level-1"
-          enabled  = var.paranoia_level == 1 ? true : false
+        dynamic "categories" {
+          for_each = local.category_settings[tostring(var.paranoia_level)]
+          content {
+            category = categories.value.category
+            enabled  = categories.value.enabled
+          }
         }
-        categories {
-          category = "paranoia-level-2"
-          enabled  = var.paranoia_level == 2 ? true : false
-        }
-        categories {
-          category = "paranoia-level-3"
-          enabled  = var.paranoia_level == 3 ? true : false
-        }
-        categories {
-          category = "paranoia-level-4"
-          enabled  = var.paranoia_level == 4 ? true : false
-        }
+
         rules {
           id              = "6179ae15870a4bb7b2d480d4843b323c"
           action          = var.owasp_action
@@ -63,59 +76,3 @@ resource "cloudflare_ruleset" "zone_level_managed_waf" {
     enabled     = var.owasp_enabled
   }
 }
-
-##      What I want the code to do
-##    
-##      paranoia = var.paranoia_level
-##    
-##      if paranoia_level == 3 then overides = XYZ
-##    
-##      if paranoia_level == 4 then overides = XYZ
-##    
-##      ETC
-##    
-##    What I think the API wants
-##    overrides {
-##    
-##      categories {
-##        category = "paranoia-level-2"
-##        action   = "block"
-##        enabled  = var.paranoia_level == 1 ? false : null
-##      }
-##      categories {
-##        category = "paranoia-level-3"
-##        action   = "block"
-##        enabled  = var.paranoia_level == 1:2 ? false : null
-##      }
-##      categories {
-##        category = "paranoia-level-4"
-##        action   = "block"
-##        enabled  = var.paranoia_level == 1:2:3 ? false : null
-##      }
-##    }
-##    
-##    What I hope works
-##    
-##    overrides {
-##    
-##      categories {
-##        category = "paranoia-level-1"
-##        action   = "block"
-##        enabled  = var.paranoia_level == 1 ? true : false
-##      }
-##      categories {
-##        category = "paranoia-level-2"
-##        action   = "block"
-##        enabled  = var.paranoia_level == 2 ? true : false
-##      }
-##      categories {
-##        category = "paranoia-level-3"
-##        action   = "block"
-##        enabled  = var.paranoia_level == 3 ? true : false
-##      }
-##      categories {
-##        category = "paranoia-level-4"
-##        action   = "block"
-##        enabled  = var.paranoia_level == 4 ? true : false
-##      }
-##    }
